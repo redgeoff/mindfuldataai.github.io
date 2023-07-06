ae0416189334c7b4bced8715e3c3883b3e1b0430
redgeoff <redgeoff@users.noreply.github.com>
2023-07-06 18:42:41 +0000

    also preserve utm_source redgeoff/mindful-data-web@fbba499462b32d203652b363e8f95e978b2fe265

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,22 @@
 function preserveUTMParameters() {
-  // Get the value of the 'utm_campaign' query string parameter
+  // Get the value of the 'utm_campaign' and 'utm_source' query string parameters
   const urlParams = new URLSearchParams(window.location.search);
   const utmCampaign = urlParams.get('utm_campaign');
+  const utmSource = urlParams.get('utm_source');
 
   // Get all hyperlinks on the page
   const links = document.getElementsByTagName('a');
 
-  // Loop through each hyperlink and update the 'utm_campaign' parameter
+  // Loop through each hyperlink and update the 'utm_campaign' and 'utm_source' parameters
   for (let i = 0; i < links.length; i++) {
     const link = links[i];
     let href = link.href;
 
     // Check if the hyperlink already contains query parameters
     if (href.includes('?')) {
-      href += `&utm_campaign=${utmCampaign}`;
+      href += `&utm_campaign=${utmCampaign}&utm_source=${utmSource}`;
     } else {
-      href += `?utm_campaign=${utmCampaign}`;
+      href += `?utm_campaign=${utmCampaign}&utm_source=${utmSource}`;
     }
 
     // Set the updated href value for the hyperlink
